6ee1909195ffb87d19c7d1b5e7641199f837b5fd
scroutchy <stef.crombez@gmail.com>
2025-03-19 07:57:18 -0300

    fix: Declare PageableWebConfig as open class

--- commons-cinema/src/main/kotlin/com/scr/project/commons/cinema/config/PageableWebConfig.kt
+++ commons-cinema/src/main/kotlin/com/scr/project/commons/cinema/config/PageableWebConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.result.method.annotation.ArgumentResolve
 
 //TODO: Investigate why need of this class in order to correctly orientate the choice of resolveArguments to the one of ReactivePageableHandlerMethodArgumentResolver
 @Configuration
-class PageableWebConfig : WebFluxConfigurer {
+open class PageableWebConfig : WebFluxConfigurer {
 
     override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
         configurer.addCustomResolver(ReactivePageableHandlerMethodArgumentResolver())
