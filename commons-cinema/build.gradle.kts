c787c5dc7bad841a7265c2be75b2d92ac53905b0
scroutchy <stef.crombez@gmail.com>
2025-04-25 18:25:36 -0300

    build: Upgrade Spring Boot to version 3.4.5

--- commons-cinema/build.gradle.kts
+++ commons-cinema/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.25"
-    id("org.springframework.boot") version "3.4.4"
+    id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
     id("java-library")
     id("maven-publish")
