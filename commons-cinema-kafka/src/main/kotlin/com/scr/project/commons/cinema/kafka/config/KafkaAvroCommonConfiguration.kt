e10b975715193490d476c2f662e39bf6df3ddf03
scroutchy <stef.crombez@gmail.com>
2025-05-05 17:18:46 -0300

    feat: Refactor KafkaAvro configuration classes to use companion object for property retrieval

--- commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroCommonConfiguration.kt
+++ commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroCommonConfiguration.kt
@@ -5,33 +5,31 @@ import org.apache.kafka.clients.CommonClientConfigs.SECURITY_PROTOCOL_CONFIG
 import org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG
 import org.apache.kafka.common.config.SaslConfigs.SASL_JAAS_CONFIG
 import org.apache.kafka.common.config.SaslConfigs.SASL_MECHANISM
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
-@Configuration
-open class KafkaAvroCommonConfiguration(
-    @Value("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String,
-    @Value("\${spring.kafka.properties.schema.registry.url}") private val schemaRegistryUrl: String,
-    @Value("\${spring.kafka.security-protocol}") private val securityProtocol: String,
-    @Value("\${spring.kafka.sasl.mechanism}") private val saslMechanism: String,
-    @Value("\${spring.kafka.sasl.username}") private val saslUsername: String?,
-    @Value("\${spring.kafka.sasl.password}") private val saslPassword: String?,
-) {
+class KafkaAvroCommonConfiguration {
 
-    @Bean
-    open fun kafkaAvroCommonProperties(): Map<String, Any> {
-        val commonProperties = mapOf(
-            BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
-            SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl,
-            SECURITY_PROTOCOL_CONFIG to securityProtocol,
-            SASL_MECHANISM to saslMechanism,
-        )
+    companion object {
 
-        return if (saslUsername != null && saslPassword != null) {
-            commonProperties + mapOf(SASL_JAAS_CONFIG to "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$saslUsername\" password=\"$saslPassword\";")
-        } else {
-            commonProperties
+        fun kafkaAvroCommonProperties(
+            bootstrapServers: String,
+            schemaRegistryUrl: String,
+            securityProtocol: String,
+            saslMechanism: String,
+            saslUsername: String?,
+            saslPassword: String?
+        ): Map<String, Any> {
+            val commonProperties = mapOf(
+                BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+                SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl,
+                SECURITY_PROTOCOL_CONFIG to securityProtocol,
+                SASL_MECHANISM to saslMechanism,
+            )
+
+            return if (saslUsername != null && saslPassword != null) {
+                commonProperties + mapOf(SASL_JAAS_CONFIG to "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$saslUsername\" password=\"$saslPassword\";")
+            } else {
+                commonProperties
+            }
         }
     }
 }
--- commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroProducerConfiguration.kt
+++ commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroProducerConfiguration.kt
@@ -4,16 +4,30 @@ import io.confluent.kafka.serializers.KafkaAvroSerializer
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.common.serialization.StringSerializer
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-open class KafkaAvroProducerConfiguration(@Qualifier("kafkaAvroCommonsProperties") private val kafkaAvroCommonsProperties: Map<String, Any>) {
+open class KafkaAvroProducerConfiguration(
+    @Value("\${spring.kafka.bootstrap-servers}") private val bootstrapServers: String,
+    @Value("\${spring.kafka.properties.schema.registry.url}") private val schemaRegistryUrl: String,
+    @Value("\${spring.kafka.security-protocol}") private val securityProtocol: String,
+    @Value("\${spring.kafka.sasl.mechanism}") private val saslMechanism: String,
+    @Value("\${spring.kafka.sasl.username}") private val saslUsername: String?,
+    @Value("\${spring.kafka.sasl.password}") private val saslPassword: String?,
+) {
 
     @Bean
     open fun kafkaAvroProducerProperties(): Map<String, Any> {
-        return kafkaAvroCommonsProperties + mapOf(
+        return KafkaAvroCommonConfiguration.kafkaAvroCommonProperties(
+            bootstrapServers,
+            schemaRegistryUrl,
+            securityProtocol,
+            saslMechanism,
+            saslUsername,
+            saslPassword
+        ) + mapOf(
             KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
         )
