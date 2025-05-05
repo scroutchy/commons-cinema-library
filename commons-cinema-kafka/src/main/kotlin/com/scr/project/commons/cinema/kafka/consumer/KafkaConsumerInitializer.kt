251d4a190525491c75a096d081d34d9073c1a3c4
scroutchy <stef.crombez@gmail.com>
2025-05-05 11:06:45 -0300

    feat: Add Kafka consumer initializer and processor with tests

---
+++ commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/consumer/KafkaConsumerInitializer.kt
@@ -0,0 +1,24 @@
+package com.scr.project.commons.cinema.kafka.consumer
+
+import com.scr.project.commons.cinema.kafka.processor.KafkaProcessor
+import jakarta.annotation.PostConstruct
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class KafkaConsumerInitializer(private val kafkaProcessors: List<KafkaProcessor<*>>) {
+
+    private val logger: Logger = LoggerFactory.getLogger(KafkaConsumerInitializer::class.java)
+
+    @PostConstruct
+    fun initialize() {
+        kafkaProcessors.forEach {
+            logger.info("Initializing ${it::class.simpleName} consumer")
+            it.startConsuming()
+                .doOnError { e -> logger.warn("Error while initializing ${it::class.simpleName} consumer: ${e.message}") }
+                .doOnComplete { logger.info("${it::class.simpleName} consumer initialized") }
+                .subscribe()
+        }
+    }
+}
