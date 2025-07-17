ddb02476a28c385e134360ba17b77537b66e54b0
scroutchy <stef.crombez@gmail.com>
2025-07-17 11:05:28 -0300

    feat: Enable idempotence in Kafka producer configuration

--- commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroProducerConfiguration.kt
+++ commons-cinema-kafka/src/main/kotlin/com/scr/project/commons/cinema/kafka/config/KafkaAvroProducerConfiguration.kt
@@ -1,6 +1,8 @@
 package com.scr.project.commons.cinema.kafka.config
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer
+import org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG
+import org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.common.serialization.StringSerializer
@@ -30,6 +32,10 @@ class KafkaAvroProducerConfiguration(
         ) + mapOf(
             KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
             VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
+            ENABLE_IDEMPOTENCE_CONFIG to true,
+            ACKS_CONFIG to "all",
+            "retries" to 5,
+
         )
     }
 }
